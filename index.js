fbfd7a85f8acca829b42d5cdf628ea83fb618ee7
ruizaj13 <ruizaj13@gmail.com>
2020-10-07 13:21:53 -0700

    completed all tasks

--- index.js
+++ index.js
@@ -167,8 +167,14 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(arr,flavor){
+    let filteredArray=[];
+    for (let i=0; i<arr.length; i++ ){
+        if(arr[i].includes(flavor)){
+            filteredArray.push(arr[i])
+        }
+    }
+    return filteredArray;
 }
 
 
